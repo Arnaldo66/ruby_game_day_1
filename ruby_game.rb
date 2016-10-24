8a9a9ec1048c07f96fbc8c8c0f5de77e1bd3d197
arnaud <arnauddeparseval@yahoo.fr>
2016-10-24 02:58:15 -0700

    Affiche player

--- ruby_game.rb
+++ ruby_game.rb
@@ -1,7 +1,14 @@
 
+player_x = 3
+player_y = 4
+
 for row in 1..5
 	for cols in 1..6
-		print "."
+		if row == player_y && cols == player_x
+			print "P"
+		else
+			print "."
+		end
 	end
 	puts
 end
